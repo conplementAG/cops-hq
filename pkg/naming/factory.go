fcc72b835b7ec3e9e4939a877c566c374eb1bb4f
Denis Biondic <denis.biondic@conplement.de>
2022-05-19 12:35:12 +0200

    refactor(naming service): file for factory methods per convention

---
+++ pkg/naming/factory.go
@@ -0,0 +1,36 @@
+package naming
+
+import (
+	"errors"
+	"github.com/conplementag/cops-hq/pkg/naming/patterns"
+)
+
+// New creates a new naming convention service.
+// Parameters 'context', 'region' and 'environment' are mandatory.
+//     Context should be set to the application name. In case of a complex system with multiple modules / subsystems,
+//       'module' should be set as well.
+//     Environment provides you the possibility to isolate your resources per environment, e.g. prod, int, stage, dev, etc.
+//     Regions should be in form of Azure long regions string, e.g. westeurope or northeurope.
+// Per default, normal naming convention pattern is used. If required, you can override the pattern using the
+// SetPattern() method
+func New(context string, region string, environment string, module string) (*Service, error) {
+	if context == "" {
+		return nil, errors.New("context must be provided")
+	}
+
+	if region == "" {
+		return nil, errors.New("region must be provided")
+	}
+
+	if environment == "" {
+		return nil, errors.New("environment must be provided")
+	}
+
+	return &Service{
+		pattern:     patterns.Normal,
+		context:     context,
+		module:      module,
+		region:      region,
+		environment: environment,
+	}, nil
+}
--- pkg/naming/service.go
+++ pkg/naming/service.go
@@ -1,7 +1,6 @@
 package naming
 
 import (
-	"errors"
 	"github.com/ahmetb/go-linq"
 	"github.com/conplementag/cops-hq/pkg/naming/patterns"
 	"github.com/conplementag/cops-hq/pkg/naming/regions"
@@ -15,36 +14,6 @@ type Service struct {
 	module      string
 	region      string
 	environment string
-}
-
-// New creates a new naming convention service.
-// Parameters 'context', 'region' and 'environment' are mandatory.
-//     Context should be set to the application name. In case of a complex system with multiple modules / subsystems,
-//       'module' should be set as well.
-//     Environment provides you the possibility to isolate your resources per environment, e.g. prod, int, stage, dev, etc.
-//     Regions should be in form of Azure long regions string, e.g. westeurope or northeurope.
-// Per default, normal naming convention pattern is used. If required, you can override the pattern using the
-// SetPattern() method
-func New(context string, region string, environment string, module string) (*Service, error) {
-	if context == "" {
-		return nil, errors.New("context must be provided")
-	}
-
-	if region == "" {
-		return nil, errors.New("region must be provided")
-	}
-
-	if environment == "" {
-		return nil, errors.New("environment must be provided")
-	}
-
-	return &Service{
-		pattern:     patterns.Normal,
-		context:     context,
-		module:      module,
-		region:      region,
-		environment: environment,
-	}, nil
 }
 
 // SetPattern changes the naming convention pattern to a user defined value. To set a custom pattern, combine the
