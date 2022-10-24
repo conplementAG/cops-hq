35e7759ef9102c2154d37128abe62eda1f044c03
Denis Biondic <denis.biondic@conplement.de>
2022-10-24 12:31:48 +0200

    feat: configurable RequireInfrastructureEncryption setting for terraform state storage

--- pkg/recipes/terraform/backend_storage_settings.go
+++ pkg/recipes/terraform/backend_storage_settings.go
@@ -2,16 +2,19 @@ package terraform
 
 type BackendStorageSettings struct {
 	CreateResourceGroup bool
-	Tags                map[string]string
-	BlobContainerName   string
-	BlobContainerKey    string
-	AllowedIpAddresses  []string
+	// RequireInfrastructureEncryption adds another layer of encryption to the storage account
+	RequireInfrastructureEncryption bool
+	Tags                            map[string]string
+	BlobContainerName               string
+	BlobContainerKey                string
+	AllowedIpAddresses              []string
 }
 
 var DefaultBackendStorageSettings = BackendStorageSettings{
-	CreateResourceGroup: true,
-	Tags:                map[string]string{},
-	BlobContainerName:   "tfstate",
-	BlobContainerKey:    "terraform.tfstate",
-	AllowedIpAddresses:  []string{},
+	CreateResourceGroup:             true,
+	RequireInfrastructureEncryption: true,
+	Tags:                            map[string]string{},
+	BlobContainerName:               "tfstate",
+	BlobContainerKey:                "terraform.tfstate",
+	AllowedIpAddresses:              []string{},
 }
