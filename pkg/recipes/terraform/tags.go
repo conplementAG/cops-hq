45b316eb9e0dedc7460bd68b7cc632d9d97aaf6b
huf1 <johannes.hufnagel@conplement.de>
2022-11-23 09:12:29 +0100

    fix(terraform-recipe): use ExecuteCmd for az cmds with tags argument

--- pkg/recipes/terraform/tags.go
+++ pkg/recipes/terraform/tags.go
@@ -2,17 +2,14 @@ package terraform
 
 import (
 	"fmt"
-	"strings"
 )
 
-// serializeTagsIntoAzureCliString converts the given tags from the map to a text that can be passed to an azure cli command
-// with the --tags parameter
-// The key and value will be quoted e.g. "This is a key"="This is a value" (to support key and value with spaces)
-func serializeTagsIntoAzureCliString(tags map[string]string) string {
-	var tagTexts []string
+// serializeTagsIntoCmdArgsList converts the given tags from the map to a slice that they can be passed as args to a command
+func serializeTagsIntoCmdArgsList(tags map[string]string) []string {
+	tagTexts := make([]string, 0)
 	for key, value := range tags {
-		tagTexts = append(tagTexts, fmt.Sprintf("%q=%q", key, value))
+		tagTexts = append(tagTexts, fmt.Sprintf("%s=%s", key, value))
 	}
 
-	return strings.Join(tagTexts, " ")
+	return tagTexts
 }
